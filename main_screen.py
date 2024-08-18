33c46c7cce565cf01f3e37b75eca97762fd68f95
Guilherme <guilherme.j.lopes@yfv.br>
2024-08-18 20:31:00 -0300

    add main screen

--- main_screen.py
+++ main_screen.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 from screen import screen
 
 class main_screen(screen):
@@ -6,4 +7,20 @@ class main_screen(screen):
         self.root = root
         
     def show(self):
-        pass
+        tab_control = ttk.Notebook(self.root)
+        tabs = ['MAIN', 'AMBIENTE', 'SOCIAL', 'GOVERNANÇA', 'RELATÓRIOS']
+        
+        for i in range(1, 6):
+            tab = tk.Frame(tab_control)
+            tab_control.add(tab, text=tabs[i-1])
+        
+        
+        tab_control.pack(fill=tk.Y, expand=True)
+        side_menu = tk.Frame(self.root, width=200, bg='gray')
+        side_menu.pack(side=tk.LEFT, fill=tk.Y)
+
+        # Add menu items to the side menu
+        menu_items = ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5']
+        for item in menu_items:
+            label = tk.Label(side_menu, text=item, padx=10, pady=5)
+            label.pack(anchor=tk.W)
