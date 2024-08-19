cd09fdf45b4a462b68d44538af1a058c7ec9a521
Murillowt <62622070+Murillowt@users.noreply.github.com>
2024-08-18 23:32:12 -0300

    Add files via upload
    Modificado para verificar se já existe usuario registrado

--- create_account_screen.py
+++ create_account_screen.py
@@ -9,6 +9,14 @@ class create_account_screen(screen):
     
         
     def check_fields(self, username, password, email, phone_number):
+        
+        with open('users.txt', 'r') as f:
+            conteudo = f.read()
+        # Verifica se a palavra já está no arquivo
+        if username in conteudo:
+            self.pop_up("Este usuário já está registrado")
+            return False
+        
         if len(password) < 4:
             self.pop_up('Senha muito curta')
             return False
