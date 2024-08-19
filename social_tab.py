47b4b141e5b0ea3d994fac1d658ef353b7376d62
Guilherme <guilherme.j.lopes@yfv.br>
2024-08-18 22:43:32 -0300

    move activity window to tab class

--- social_tab.py
+++ social_tab.py
@@ -5,7 +5,7 @@ from activity import activity
 
 class social_tab(tab):
     def __init__(self, root):
-        self.root = root
+        super().__init__(root)
     
     def upload_file(self):
         pass
@@ -17,43 +17,4 @@ class social_tab(tab):
         open_event_button.pack()
     
     
-    def open_event_screen(self):
-        popup_window = Toplevel(self.root)
-        titulo_label = ttk.Label(popup_window, text='Título:')
-        titulo_label.pack()
-        titulo_text_box_title = ttk.Entry(popup_window)
-        titulo_text_box_title.pack()
-        
-        valor_label = ttk.Label(popup_window, text='Valor:')
-        valor_label.pack()
-        valor_entry = ttk.Entry(popup_window)
-        valor_entry.pack()
-        
-        responsavel_label = ttk.Label(popup_window, text='Responsável:')
-        responsavel_label.pack()
-        responsavel_entry = ttk.Entry(popup_window)
-        responsavel_entry.pack()
-        
-        beneficiario_label = ttk.Label(popup_window, text='Beneficiário:')
-        beneficiario_label.pack()
-        beneficiario_entry = ttk.Entry(popup_window)
-        beneficiario_entry.pack()
-        
-        upload_button = ttk.Button(popup_window, text='Adicionar anexo', command=self.upload_file)
-        upload_button.pack(side=tk.RIGHT)
-        
-        
-        def submit():
-            titulo = titulo_text_box_title.get()
-            valor = valor_entry.get()
-            responsavel = responsavel_entry.get()
-            beneficiario = beneficiario_entry.get()
-            
-            act = activity(titulo, valor, responsavel, beneficiario)
-            act.register()
-            del act
-        
-            popup_window.destroy()
-            
-        button = ttk.Button(popup_window, text='Submit', command=submit)
-        button.pack(side=tk.RIGHT)
+    
--- tab.py
+++ tab.py
@@ -1,10 +1,53 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import Toplevel, ttk
+
+from activity import activity
+
 
 class tab:
     def __init__(self, root):
-        pass
+        self.root = root
         
     def show(self):
         pass
+    
+    def open_event_screen(self):
+        popup_window = Toplevel(self.root)
+        titulo_label = ttk.Label(popup_window, text='Título:')
+        titulo_label.pack()
+        titulo_text_box_title = ttk.Entry(popup_window)
+        titulo_text_box_title.pack()
         
+        valor_label = ttk.Label(popup_window, text='Valor:')
+        valor_label.pack()
+        valor_entry = ttk.Entry(popup_window)
+        valor_entry.pack()
+        
+        responsavel_label = ttk.Label(popup_window, text='Responsável:')
+        responsavel_label.pack()
+        responsavel_entry = ttk.Entry(popup_window)
+        responsavel_entry.pack()
+        
+        beneficiario_label = ttk.Label(popup_window, text='Beneficiário:')
+        beneficiario_label.pack()
+        beneficiario_entry = ttk.Entry(popup_window)
+        beneficiario_entry.pack()
+        
+        upload_button = ttk.Button(popup_window, text='Adicionar anexo', command=self.upload_file)
+        upload_button.pack(side=tk.RIGHT)
+        
+        
+        def submit():
+            titulo = titulo_text_box_title.get()
+            valor = valor_entry.get()
+            responsavel = responsavel_entry.get()
+            beneficiario = beneficiario_entry.get()
+            
+            act = activity(titulo, valor, responsavel, beneficiario)
+            act.register()
+            del act
+        
+            popup_window.destroy()
+            
+        button = ttk.Button(popup_window, text='Submit', command=submit)
+        button.pack(side=tk.RIGHT)
